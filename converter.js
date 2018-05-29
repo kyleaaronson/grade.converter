c972806dafe4fb15affe9d86fb5deed01d35840f
Kyle Aaronson <aaronske@dukes.jmu.edu>
2018-05-29 13:59:29 -0400

    its coming along

--- converter.js
+++ converter.js
@@ -4,21 +4,29 @@ const gradeAverage = (quiz, test, final) => {
 	return (quiz * 0.2 + test * 0.3 + final * 0.5);
 };
 
-if (0<= gradeAverage < 60) {
-	console.log("The students grade average is " + gradeAverage + "they will recieve a F in the class")
-} else if (60 <= gradeAverage < 70) {
-	console.log("The students grade average is " + gradeAverage + "they will recieve a D in the class")
-} else if (70 <= gradeAverage < 80) {
-	console.log("The students grade average is " + gradeAverage + "they will recieve a C in the class")
-} else if (80 <= gradeAverage < 90) {
-	console.log("The students grade average is " + gradeAverage + "they will recieve a B in the class")
-} else if (90 <= gradeAverage < 100) {
-	console.log("The students grade average is " + gradeAverage + "they will recieve a A in the class")
-}; else console.log('Input error')
+// converts grade average to letter grade
+const convertNumbertoGrade = (n) => {
+		if (n >= 90) {
+			return "A";
+	}	if (n >= 80) {
+			return "B"
+	}	if (n >= 70) {
+			return "C"
+	}	if (n >= 60) {
+			return "D"
+	}	return "F"
+};
 
+// outputs a message to student with grade avaerage and letter grade earned
+const getMessage = (avg, grade) => {
+	return "the student's average is " + avg + " They will recieve a " + grade + " in the class."
+}
+
+let avg = gradeAverage(98,77,84);
+let g = convertNumbertoGrade(avg);
+console.log(getMessage(avg, g));
 
 module.exports = {
 	gradeAverage
 };
-
 
--- converter.spec.js
+++ converter.spec.js
@@ -1,14 +1,28 @@
-// import code to be tested
+// smoke test
 const gc = require('./converter');
+const lc = require('./converter');
 
-describe('An average grade calculator', () => {
-	
+describe("smoke test", () => {
+	it("can blow smoke",() => {
+		expect(true).toBe(true);
+	});
+});
 
+//test conversion of grade averages
+describe("A grade average calculator", () => {
 
-
-	it("can calculate average grade", () => {
-		expect(gc.gradeAverage(0, 0, 0)).toBe(0)
+	it("can average multiple grades", () => {
+		expect(gc.gradeAverage(0,0,0)).toBe(0);
 		expect(gc.gradeAverage(60, 80, 90)).toBe(81);
 		expect(gc.gradeAverage(50, 60, 80)).toBe(68);
+	});
+});
+
+// test convert number to grade
+describe("A letter grade converter", () => {
+
+	it("can convert average grade to letter grade", () => {
+		expect(lc.convertNumberToGrade(90)).toBe(A);
+		expect(lc.convertNumberToGrade(84)).toBe(B);
 	});
 });
